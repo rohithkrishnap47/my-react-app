920ddef902718875a24650d62a5f896a5ff5bf0f
Rohith Krishna <roal2027@gmail.com>
2024-04-24 11:58:43 +0530

    Food component added

---
+++ src/Food.jsx
@@ -0,0 +1,15 @@
+
+function Fruits(){
+    const fruit1="Apple"
+    const fruit2="Gauva"
+    const fruit3="Orange"
+    return (
+        <ul>
+            <li>{fruit1}</li>
+            <li>{fruit2}</li>
+            <li>{fruit3}</li>
+            <li>random @#$%^&*</li>
+        </ul>
+    )
+}
+export default Fruits
